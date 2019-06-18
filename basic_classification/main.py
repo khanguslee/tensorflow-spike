ee21a60e8792aaf3ea128f45a7da59d177069c19
khanguslee <angus.lee809@gmail.com>
2019-06-18 17:04:30 +1000

    Setup initial files

---
+++ basic_classification/main.py
@@ -0,0 +1,13 @@
+"""
+Tutorial:
+    https://www.tensorflow.org/tutorials/keras/basic_classification#preprocess_the_data
+"""
+
+from __future__ import absolute_import, division, print_function
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+print(tf.__version__)
